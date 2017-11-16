fc1ec4d5d8ede65cd9a69f85f90ac622d181fddf
keer2345 <keer2345@gmail.com>
2017-11-16 21:51:37 +0800

    Django认证内置的登录登出视图

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,9 +1,15 @@
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 from account.forms import LoginForm
+
+
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
 
 
 def user_login(request):
