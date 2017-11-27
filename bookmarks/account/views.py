96fc46c44557965e72883181bba32e4efd61c542
keer2345 <keer2345@gmail.com>
2017-11-27 20:32:15 +0800

    添加消息框架

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
@@ -54,6 +55,7 @@ def register(request):
                   'account/register.html',
                   {'user_form': user_form})
 
+
 @login_required
 def edit(request):
     if request.method == 'POST':
@@ -65,6 +67,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
