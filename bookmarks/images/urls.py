0f7b64716eae14120c7447c26cb213b67412e17d
keer2345 <keer2345@gmail.com>
2017-12-23 10:54:12 +0800

    ajax (like or unlike a image)

--- bookmarks/images/urls.py
+++ bookmarks/images/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^create_pre/$', views.image_create_pre, name='create_pre'),
     url(r'^create/$', views.image_create, name='create'),
     url(r'^detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.image_detail, name='detail'),
+    url(r'^like/$', views.image_like, name='like'),
 ]
